464b5a7eef66fad44f7d7647c766089498debd37
Fernando Salazar <fslzrr@gmail.com>
2020-05-28 04:23:52 -0500

    preparing for deployment

--- back/config.js
+++ back/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   DATABASE_URL:
     process.env.DATABASE_URL ||
-    // "mongodb+srv://admin:Pass.word1@cluster0-0b5w8.mongodb.net/fslzrr-blog?retryWrites=true&w=majority" ||
+    "mongodb+srv://admin:Pass.word1@cluster0-0b5w8.mongodb.net/fslzrr-blog?retryWrites=true&w=majority" ||
     "mongodb://localhost/fslzrr-blog",
   PORT: process.env.PORT || 8080,
   JWT: {
