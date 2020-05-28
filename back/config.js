cdbf8bcce512d6839f2c05d376d8319af45bc906
Fernando Salazar <fslzrr@gmail.com>
2020-05-28 05:51:33 -0500

    prepared backend with env vars

--- back/config.js
+++ back/config.js
@@ -1,13 +1,10 @@
 const CONFIG = {
-  DATABASE_URL:
-    process.env.DATABASE_URL ||
-    "mongodb+srv://admin:Pass.word1@cluster0-0b5w8.mongodb.net/fslzrr-blog?retryWrites=true&w=majority" ||
-    "mongodb://localhost/fslzrr-blog",
-  PORT: process.env.PORT || 8080,
+  DATABASE_URL: process.env.DATABASE_URL,
+  PORT: process.env.PORT,
   JWT: {
     credentialsRequired: true,
-    secret: process.env.SECRET || "why-so-serioussss",
-    usecurePaths: ["", "/", "/user", "/auth/login"],
+    secret: process.env.SECRET,
+    usecurePaths: ["", "/user", "/auth/login"],
   },
 };
 
